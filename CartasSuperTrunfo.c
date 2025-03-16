f24606c2768ee686d7687951c9e4c7532f2e68e7
Matheus R. Ouverney <162365159+Matheuzin18@users.noreply.github.com>
2025-03-16 18:04:47 +0000

    Cartas Super Trunfo Nivel Mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,6 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
 
@@ -23,7 +22,7 @@ int main() {
     scanf("%s", &cidade);
     // Cidade que a carta representa
 
-    int populacao;
+    unsigned long int populacao;
     printf("Digite o número aproximado de habitantes da cidade: \n");
     scanf("%i", &populacao);
     // Número de habitantes que a cidade possui
@@ -49,7 +48,11 @@ int main() {
     float pibPerCapita = pib / populacao;
     // Calcular o PIB per capita da cidade
 
+    float DensidadePoder = area / populacao;
+    // Calcular o inverso da densidade para calcular o valor do Super Poder da carta
 
+    float SuperPoder = (float) populacao + area + pib + turistico + pibPerCapita + DensidadePoder;
+    // Calcular o valor do Super Poder da carta
 
 
     char estado2;
@@ -67,7 +70,7 @@ int main() {
     scanf("%s", &cidade2);
     // Cidade que a carta representa
     
-    int populacao2;
+    unsigned long int populacao2;
     printf("Digite o número aproximado de habitantes da nova cidade: \n");
     scanf("%i", &populacao2);
     // Número de habitantes que a cidade possui
@@ -93,10 +96,16 @@ int main() {
     float pibPerCapita2 = pib2 / populacao2;
     // Calcular o PIB per capita da cidade
 
+    float DensidadePoder2 = area2 / populacao2;
+    // Calcular o inverso da densidade para calcular o valor do Super Poder da carta
+
+    float SuperPoder2 = (float) populacao2 + area2 + pib2 + turistico2 + pibPerCapita2 + DensidadePoder2;
+    // Calcular o valor do Super Poder da carta
+
     printf("A letra que representa o primeiro estado é: %c. \n", estado);
     printf("O código da primeira carta é: %s. \n", codigo);
     printf("O nome da cidade é: %s. \n", cidade);
-    printf("A população da cidade é de aproximadamente %i habitantes. \n", populacao);
+    printf("A população da cidade é de aproximadamente %lu habitantes. \n", populacao);
     printf("A área da cidade é de: %.1fkm². \n", area);
     printf("O PIB da cidade é de: R$%.1f. \n", pib);
     printf("A cidade tem um total de %i pontos turísticos. \n", turistico);
@@ -107,13 +116,35 @@ int main() {
     printf("A letra que representa o segundo estado é: %c. \n", estado2);
     printf("O código da segunda carta é: %s. \n", codigo2);
     printf("O nome da segunda cidade é: %s. \n", cidade2);
-    printf("A população da outra cidade é de aproximadamente %i habitantes. \n", populacao2);
+    printf("A população da outra cidade é de aproximadamente %lu habitantes. \n", populacao2);
     printf("A área da cidade é de: %.1fkm². \n", area2);
     printf("O PIB da cidade é de: R$%.1f. \n", pib2);
     printf("Essa cidade tem um total de %i pontos turísticos. \n", turistico2);
     printf("A densidade populacional dessa cidade é de %.2f habitantes por km². \n", DensidadePopulacional2);
-    printf("O pib per capita dessa cidade é de R$%.2f. \n", pibPerCapita2);
+    printf("O pib per capita dessa cidade é de R$%.2f. \n\n", pibPerCapita2);
     //Exibir todas as informações referentes a segunda carta
+
+
+
+    printf("***Comparação Cartas Super Trunfo***\n");
+    printf("***Obs: o valor 0 corresponde que a carta vencedora foi a segunda e o valor 1 corresponde que a carta vencedora foi a primeira!***\n");
+    //Introdução e breve explicação de como funciona a comparação
+
+    int resultadoPopulacao = populacao > populacao2;
+    printf("A carta vencedora no atributo POPULAÇÃO foi:%d.\n", resultadoPopulacao);
+    int resultadoArea = area > area2;
+    printf("A carta vencedora no atributo ÁREA foi:%d.\n", resultadoArea);
+    int resultadoPib = pib > pib2;
+    printf("A carta vencedora no atributo PIB foi:%d.\n", resultadoPib);
+    int resultadoTuristico = turistico > turistico2;
+    printf("A carta vencedora no atributo PONTOS TURÍSTICOS foi:%d.\n", resultadoTuristico);
+    int resultadoDensidade = DensidadePopulacional > DensidadePopulacional2;
+    printf("A carta vencedora no atributo DENSIDADE POPULACIONAL foi:%d.\n", resultadoDensidade);
+    int resultadoPibPerCapita = pibPerCapita > pibPerCapita2;
+    printf("A carta vencedora no atributo PIB PER CAPITA foi:%d.\n", resultadoPibPerCapita);
+    int resultadoSuperPoder = SuperPoder > SuperPoder2;
+    printf("A carta vencedora no atributo SUPER PODER foi:%d.\n", resultadoSuperPoder);
+    //Cálculos de qual carta vence em cada atributo
 
     return 0;
 
